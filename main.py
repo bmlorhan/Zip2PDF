fbda07d692ffca9c668f8dac49e97af1ab3d7f00
cenie <bmlorhan@gmail.com>
2021-03-16 17:57:08 -0700

    # Version 0.3.1 # ZIP extraction can now select and extract multiple files # Added Exit Application button.

--- main.py
+++ main.py
@@ -5,6 +5,9 @@
 # GUI added using tkinter
 # Functional extraction with select file and extract buttons
 
+# Version 0.3.1
+# ZIP extraction can now select and extract multiple files
+# Added Exit Application button.
 
 # required modules
 import errno
@@ -66,43 +69,45 @@ canvas1.create_window(325, 190, window=convertImageButton)
 
 
 def selectZipFile():
-    global zip_file_path
-    zip_file_path = filedialog.askopenfilename()
+    global zip_file_path_list
+    zip_file_path = filedialog.askopenfilenames()
+    zip_file_path_list = list(zip_file_path)
 
 
 # extract function
 def extractZipFile():
     # get zip file name
-    zip_folder_name = zip_file_path
-
+    zip_folder_name = zip_file_path_list
+    print(zip_folder_name)
     # path and directory for extracted files.
     # Creates directory in the same location as ZIP file, under the same name. Removes '.zip'
-    directory = zip_folder_name.replace('.zip', '')
 
-    with ZipFile(zip_folder_name, 'r') as zip_ref:
-        for f in zip_ref.infolist():
-            bad_filename = f.filename
-            if bytes != str:
-                bad_filename = bytes(bad_filename, 'cp437')
+    for file_path in zip_folder_name:
+        directory = file_path.replace('.zip', '')
+        with ZipFile(file_path, 'r') as zip_ref:
+            for f in zip_ref.infolist():
+                bad_filename = f.filename
+                if bytes != str:
+                    bad_filename = bytes(bad_filename, 'cp437')
 
-            # decode to sjis
-            try:
-                uf = codecs.decode(bad_filename, 'sjis')
-            except UnicodeDecodeError:
-                print('uf did not decode to "sjis", attempting to decode to shift_jisx0213. ')
-                uf = codecs.decode(bad_filename, 'shift_jisx0213')
-
-            filename = os.path.join(directory, uf)
-            if not os.path.exists(os.path.dirname(filename)):
+                # decode to sjis
                 try:
-                    os.makedirs(os.path.dirname(filename))
-                except OSError as exc:
-                    if exc.errno != errno.EEXIST:
-                        raise
+                    uf = codecs.decode(bad_filename, 'sjis')
+                except UnicodeDecodeError:
+                    print('uf did not decode to "sjis", attempting to decode to shift_jisx0213. ')
+                    uf = codecs.decode(bad_filename, 'shift_jisx0213')
 
-            if not filename.endswith('/'):
-                with open(filename, 'wb') as dest:
-                    dest.write(zip_ref.read(f))
+                filename = os.path.join(directory, uf)
+                if not os.path.exists(os.path.dirname(filename)):
+                    try:
+                        os.makedirs(os.path.dirname(filename))
+                    except OSError as exc:
+                        if exc.errno != errno.EEXIST:
+                            raise
+
+                if not filename.endswith('/'):
+                    with open(filename, 'wb') as dest:
+                        dest.write(zip_ref.read(f))
 
 
 # Button for selecting ZIP to extract
@@ -115,6 +120,11 @@ extractZipButton = tk.Button(text="Extract", command=extractZipFile, bg='green',
                              font=('helvetica', 12, 'bold'))
 canvas1.create_window(125, 190, window=extractZipButton)
 
+
+# Close application button
+closeApplicationButton = tk.Button(text="Close Application", command=root.destroy, bg='red', fg='white',
+                                   font=('helvetica', 12, 'bold'))
+canvas1.create_window(235, 300, window=closeApplicationButton)
 
 if __name__ == "__main__":
     root.mainloop()
